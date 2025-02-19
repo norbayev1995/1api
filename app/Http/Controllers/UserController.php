30e4053a1d6b4b5dc05d69ab4462e914b80120df
Mansur <ttytkhkttymi@gmail.com>
2025-02-19 12:20:10 +0500

    Refactored store logic in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,11 +24,7 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->save();
+        $user = User::create($request->validated());
         return response()->json($user, 200);
     }
 
